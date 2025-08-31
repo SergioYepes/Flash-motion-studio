dfe2bdde69c7f0df128b0b820b6652a9f5541bcb
unknown <elgatobanban@gmail.com>
2025-08-30 19:06:56 -0500

    fix: build services as Services

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,7 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import About from "../components/About";
-import Services from "../components/services";
+import Services from "../components/Services";
 import Footer from "../components/Footer";
 
 const Index = () => {
